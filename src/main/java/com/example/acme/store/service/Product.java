6da654b24974784db489e5af62aec867899ebadc
Sergi Trujillo <sergi.trujillo@gmail.com>
2021-10-21 09:58:59 +0200

    changes from code review

--- src/main/java/com/example/acme/store/service/Product.java
+++ src/main/java/com/example/acme/store/service/Product.java
@@ -2,12 +2,13 @@ package com.example.acme.store.service;
 
 import javax.json.bind.annotation.JsonbVisibility;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @JsonbVisibility(FieldPropertyVisibilityStrategy.class)
 public class Product {
-    @NotNull(message = "Name mandatory")
+    @NotBlank(message = "Name mandatory")
     @Size(max = 100, message = "name should be at max 100 chars")
     private String name;
 
--- src/main/java/com/example/acme/store/service/ProductController.java
+++ src/main/java/com/example/acme/store/service/ProductController.java
@@ -10,7 +10,7 @@ import java.net.URI;
 import java.util.Collection;
 import java.util.Objects;
 
-@Path("product")
+@Path("products")
 @RequestScoped
 public class ProductController {
 
